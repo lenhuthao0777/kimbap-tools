0474b2a6ff37f5d54a5429d2bda4ff7235e0166c
LeNhutHao <lenhuthao0777@gmail.com>
2024-01-28 17:36:17 +0700

    init source

---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,7 @@
+import Test from './test';
+
+const Page = () => {
+  return <Test />;
+};
+
+export default Page;
---
+++ apps/web/app/test/test.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Button } from '@kimbap-ui';
+const Test = () => {
+  return (
+    <div className='flex flex-col space-y-5'>
+      <div>
+        <Button className='text-xs'>Click!</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
